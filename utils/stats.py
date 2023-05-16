9d0485f92a1543803086df04bb430813ebc3ad32
Valurauta <wncjf2000@naver.com>
2023-05-17 00:54:18 +0900

    3

--- utils/stats.py
+++ utils/stats.py
@@ -11,10 +11,11 @@ def count_files(directory):
 def update_readme(file_counts):
     info = f"### 누적 기록\n"
     total_count = 0
+    for count in file_counts.items():
+        total_count += count
     info += f"#### 총 합계: {total_count}\n"
     for directory, count in file_counts.items():
         info += f"- [{directory}]: {count}개 해결\n"
-        total_count += count
 
     readme = f"""# Javascript CodingTest
 ### _자바스크립트 코딩테스트_
@@ -28,12 +29,13 @@ def update_readme(file_counts):
 | 사용 언어 | JavaScript |
 | 개발 도구 | Visual Studio Code |
 | OS 환경 | Windows 10 |
+
    [Javascript]: <https://developer.mozilla.org/ko/docs/Web/JavaScript>
-   [Baekjoon]: <https://www.acmicpc.net/>
-   [Programmers]: <https://programmers.co.kr/>
+   [./Baekjoon]: <https://www.acmicpc.net/>
+   [./Programmers]: <https://programmers.co.kr/>
 """
 
-    with open("./README.md", 'w', encoding='utf-8') as f:
+    with open("./README.MD", 'w', encoding='utf-8') as f:
         f.write(readme)
 
 
