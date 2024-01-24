e9df7c4cd3c408d8e33b0caec8df3fa9981fef88
Valurauta <valurauta628@gmail.com>
2024-01-24 23:03:52 +0900

    01/24

--- Programmers/Lv1/147355.js
+++ Programmers/Lv1/147355.js
@@ -1,8 +1,20 @@
 //크기가 작은 부분문자열
 function solution(t, p) {
+    let answer = 0;
+    for(let i = 0; i <= t.length - p.length; i++) {
+        if (+t.slice(i, p.length + i) <= +p) {
+            answer++;
+        }
+    }
+    return answer;
+}
+
+/*
+function solution(t, p) {
     var answer = 0;
     for(var i = 0;i<=t.length-p.length;i++)
         if (parseInt(t.slice(i, i + p.length)) <= parseInt(p)) 
             answer++;
     return answer;
 }
+*/
